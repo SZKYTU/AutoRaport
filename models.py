2c1d0b0471144db1b73e8a19a9cee280ce4ed27a
Szkytu_Work <szkycode@gmail.com>
2023-03-06 14:58:47 +0100

    models beta

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, Text, ForeignKey, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Date, Text, ForeignKey, DateTime, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
@@ -22,27 +22,29 @@ engine = create_engine('mssql+pyodbc://', creator=lambda: conn)
 Base = declarative_base()
 
 
-class Protocol(Base):
-    __tablename__ = 'protocols'
-
-    id = Column(Integer, primary_key=True)
-    date = Column(Date, default=text('CURRENT_TIMESTAMP'))
-    first_name = Column(String(50), nullable=False)
-    last_name = Column(String(50), nullable=False)
-    laptops = relationship("Laptop", backref="protocol")
-    charger = Column(Boolean, default=False)
-    comment = Column(String(250))
-
-
 class Laptop(Base):
     __tablename__ = 'laptops'
 
     id = Column(Integer, primary_key=True)
     serial_number = Column(String(50), nullable=False)
-    protocol_id = Column(Integer, ForeignKey('protocols.id'), nullable=False)
     model = Column(String(50), nullable=False)
+    cement = Column(String(50))
+    company = Column(String(50), nullable=False)
     status = Column(String(50), nullable=False)
-    comment = Column(Text)
+
+
+class Protokol(Base):
+    __tablename__ = 'protocols'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime, nullable=False)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    laptop_id = Column(Integer, ForeignKey('laptops.id'), nullable=False)
+    charger = Column(String(50))
+    coment = Column(String(200))
+    scan = Column(LargeBinary)
+    laptop = relationship("Laptop")
 
 
 Base.metadata.create_all(engine)
