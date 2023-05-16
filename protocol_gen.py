01c4830de51f1330c6089954f9ba467bc76c0dea
Piotr Czupyk <piotrczupyk@Piotrs-Mac-mini.local>
2023-05-16 18:44:51 +0200

    pdfgenerator work

--- protocol_gen.py
+++ protocol_gen.py
@@ -1,30 +1,41 @@
 from reportlab.pdfgen import canvas as pdf_canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
-from reportlab.lib.utils import ImageReader
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
+from datetime import datetime
 
 
-def generate_pdf(model_laptopa, numer_seryjny, odbiorca, nadawca):
-    pdf_name = "protokol.pdf"
-    header = "Protokół zdawczo/odbiorczy"
-    sentence = f"Ja {odbiorca} odbieram poniższy sprzęt"
+def generate_pdf(model_laptop, serial_number, pracownik, typ):
+    pdf_name = "protocol.pdf"
+    header = "Delivery/Receipt Protocol"
+    sentence = f"Ja {pracownik} odbieram poniższy sprzęt: "
+    current_date = datetime.now().strftime("%d/%m/%y")
 
-    # Rejestracja czcionki Liberation
-    pdfmetrics.registerFont(TTFont('LiberationSans', 'LiberationSans-Regular.ttf'))
-    pdfmetrics.registerFont(TTFont('LiberationSans-Bold', 'LiberationSans-Bold.ttf'))
+    # pdfmetrics.registerFont(
+    #     TTFont('LiberationSans', 'LiberationSans-Regular.ttf'))
+    # pdfmetrics.registerFont(
+    #     TTFont('LiberationSans-Bold', 'LiberationSans-Bold.ttf'))
 
     canvas = pdf_canvas.Canvas(pdf_name, pagesize=letter)
-    canvas.setFont("LiberationSans-Bold", 16)
+    canvas.setFont("Helvetica-Bold", 16)
     canvas.drawCentredString(4.25*inch, 10.5*inch - 0.5*inch, header)
-    canvas.setFont("LiberationSans", 12)
-    canvas.drawString(1*inch, 10.5*inch - 1.5*inch, sentence)
-    canvas.drawString(1*inch, 10.5*inch - 1.75*inch, f"Model laptopa: {model_laptopa}")
-    canvas.drawString(1*inch, 10.5*inch - 2*inch, f"Numer seryjny: {numer_seryjny}")
-    canvas.drawString(1*inch, 10.5*inch - 2.25*inch, f"Odbiorca: {odbiorca}")
-    canvas.drawString(1*inch, 10.5*inch - 2.5*inch, f"Nadawca: {nadawca}")
+    canvas.setFont("Helvetica", 12)
+    canvas.drawString(1*inch, 10.5*inch - 1.75*inch, sentence)
+    canvas.drawString(1*inch, 10.5*inch - 2.25*inch,
+                      f"Laptop Model: {model_laptop}")
+    canvas.drawString(1*inch, 10.5*inch - 2.5*inch,
+                      f"Serial Number: {serial_number}")
+    canvas.drawString(1*inch, 10.5*inch - 2.75*inch,
+                      f"Date: {current_date}")
+
+    wydajacy_x = 1 * inch
+    wydajacy_y = 1.5 * inch
+
+    odbiorca_x = letter[0] - \
+        canvas.stringWidth("Recipient", "Helvetica", 12) - 1 * inch
+    odbiorca_y = 1.5 * inch
+
+    canvas.drawString(wydajacy_x, wydajacy_y, "Issuer")
+    canvas.drawString(odbiorca_x, odbiorca_y, "Recipient")
+
     canvas.showPage()
     canvas.save()
-
-generate_pdf("Asus VivoBook S14", "12345", "Jan Kowalski", "Anna Nowak")
