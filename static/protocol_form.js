c9ee20cc87b7b6174c277c4a2ac946bad721b9cc
Szkytu_Work <szkycode@gmail.com>
2023-07-28 14:02:14 +0200

    refactor

---
+++ static/protocol_form.js
@@ -0,0 +1,41 @@
+const showModalWithOptions = (id) => {
+  const options = {
+    title: "Podgląd protokołów",
+    confirmButtonText: "Pobierz protokół odbiorczy",
+    showDenyButton: true,
+    denyButtonText: "Pobierz protokół zdawczy",
+    focusDeny: true,
+  };
+
+  Swal.fire(options).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = `/protocol/download/${id}/receiving`;
+    } else if (result.isDenied) {
+      window.location.href = `/protocol/download/${id}/delivery`;
+    }
+  });
+};
+
+const protocolTable = (id) => {
+  const protocol_id = id;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", `/protocol/${protocol_id}`);
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const protocol = JSON.parse(xhr.responseText);
+      const tableBody = document.querySelector("#protocolTable tbody");
+      const row = document.createElement("tr");
+      row.innerHTML = `
+            <td>${protocol.laptop.company}</td>
+            <td>${protocol.laptop.model}</td>
+            <td>${protocol.laptop.serial_number}</td>
+            <td>${protocol.protocol.last_name}</td>
+            <td>${protocol.protocol.date}</td>
+          `;
+      tableBody.appendChild(row);
+    } else {
+      console.error(xhr.statusText);
+    }
+  };
+  xhr.send();
+};
